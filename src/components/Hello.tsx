c377895a422516ec9cd8e9298dfad7c3d1e0e562
maruyama.shotaro <maruyama.shotaro@Donuts-N587.local>
2018-11-14 10:38:27 +0900

    use redux

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -4,47 +4,67 @@ import './Hello.css'
 export interface Props {
     name: string;
     enthusiasmLevel?: number;
+    onIncrement?: () => void;
+    onDecrement?: () => void;
 }
 
-interface State {
-    currentEnthusiasm: number;
-}
+// interface State {
+//     currentEnthusiasm: number;
+// }
 
-class Hello extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = { currentEnthusiasm: props.enthusiasmLevel || 1 };
-    }
+// class Hello extends React.Component<Props, State> {
+//     constructor(props: Props) {
+//         super(props);
+//         this.state = { currentEnthusiasm: props.enthusiasmLevel || 1 };
+//     }
 
-    onIncrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm + 1);
-    onDecrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm - 1);
+//     onIncrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm + 1);
+//     onDecrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm - 1);
 
-    render() {
-        const { name } = this.props;
+//     render() {
+//         const { name } = this.props;
 
-        if (this.state.currentEnthusiasm <= 0) {
-            throw new Error('You could be a little more enthusiastic. :D');
+//         if (this.state.currentEnthusiasm <= 0) {
+//             throw new Error('You could be a little more enthusiastic. :D');
 
-        }
+//         }
 
-        return (
-            <div className="hello">
-                <div className="greeting">
-                    Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
-                </div>
-                <button onClick={this.onDecrement}>-</button>
-                <button onClick={this.onIncrement}>+</button>
-            </div>
-        );
-    }
+//         return (
+//             <div className="hello">
+//                 <div className="greeting">
+//                     Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
+//                 </div>
+//                 <button onClick={this.onDecrement}>-</button>
+//                 <button onClick={this.onIncrement}>+</button>
+//             </div>
+//         );
+//     }
 
-    updateEnthusiasm(currentEnthusiasm: number) {
-        this.setState({ currentEnthusiasm });
-    }
-}
+//     updateEnthusiasm(currentEnthusiasm: number) {
+//         this.setState({ currentEnthusiasm });
+//     }
+// }
 
 export default Hello;
 
 function getExclamationMarks(numChars: number) {
     return Array(numChars + 1).join('!');
+}
+
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
+    if (enthusiasmLevel <= 0) {
+        throw new Error('You could be a little more enthusiastic. :D');
+    }
+
+    return (
+        <div className="hello" >
+            <div className="greeting">
+                Hello {name + getExclamationMarks(enthusiasmLevel)}
+            </div>
+            <div>
+                <button onClick={onDecrement}>-</button>
+                <button onClick={onIncrement}>+</button>
+            </div>
+        </div>
+    );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Hello from "./components/Hello";
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+import { EnthusiasmAction } from './actions/index';
+
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
